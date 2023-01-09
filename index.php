d48a78cf85c8e1f922a9ff32638a30869236947b
19nikilangiu91 <niki.langiu@gmail.com>
2023-01-09 16:37:15 +0100

    Point Two Done

--- index.php
+++ index.php
@@ -21,6 +21,20 @@
         echo "La lunghezza del paragrafo è di: " . strlen($paraOne) . " parole.";
         ?>
     </h2>
+
+    <!-- 2. Una parola da censurare viene passata dall'utente tramite parametro GET: 
+        - Stampare di nuovo il paragrafo e la sua lunghezza, dopo aver sostituito con tre asterischi tutte le occorrenze della parola da censurare. -->
+    <h1>
+        <?php
+        $badWord = $_GET["word"];
+        echo $paraTwo = str_replace($badWord, "***", $paraOne);
+        ?>
+    </h1>
+    <h2>
+        <?php
+        echo "La lunghezza del paragrafo è di: " . strlen($paraTwo) . " parole.";
+        ?>
+    </h2>
 </body>
 
 </html>
